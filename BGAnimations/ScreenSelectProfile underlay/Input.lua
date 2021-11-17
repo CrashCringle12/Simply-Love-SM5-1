bae5f5f30a2fd8d64d4085d906174688040f2287
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2021-11-17 18:07:26 -0500

    Add support for Card Swipes
    (cherry picked from commit 45011ff303ceeda8e7973f0e73151d20e3fdfd72)
    
    # Conflicts:
    #	BGAnimations/ScreenSelectProfile underlay/Input.lua

--- BGAnimations/ScreenSelectProfile underlay/Input.lua
+++ BGAnimations/ScreenSelectProfile underlay/Input.lua
@@ -3,6 +3,8 @@ local af = args.af
 local scrollers = args.Scrollers
 local localprofile_data = args.ProfileData
 local guest_data = args.GuestData
+local str = ""
+local time = GetTimeSinceStart()
 
 -- a simple boolean flag we'll use to ignore input once profiles have been
 -- selected and the screen's OffCommand has been queued.
@@ -107,6 +109,7 @@ Handle.MenuLeft = function(event)
 		if index - 1 > -1 then
 			MESSAGEMAN:Broadcast("DirectionButton")
 			scrollers[event.PlayerNumber]:scroll_by_amount(-1)
+
 			local data = profile_data[index+index_padding-1]
 			local frame = af:GetChild(ToEnumShortString(event.PlayerNumber) .. 'Frame')
 			frame:GetChild("SelectedProfileText"):settext(data and data.displayname or "")
@@ -160,25 +163,41 @@ Handle.Back = function(event)
 		-- "Unjoin this player and unmount their USB stick if there is one"
 		-- see ScreenSelectProfile.cpp for details
 		SCREENMAN:GetTopScreen():SetProfileIndex(event.PlayerNumber, -2)
-
 		-- CurrentStyle has to be explicitly set to single in order to be able to
 		-- unjoin a player from a 2-player setup
 		if SL.Global.FastProfileSwitchInProgress and GAMESTATE:GetNumSidesJoined() == 1 then
 			GAMESTATE:SetCurrentStyle("single")
 			SCREENMAN:GetTopScreen():playcommand("Update")
 		end
-
 	end
 end
 
-
+local potentialSwipe = false
 local InputHandler = function(event)
 	if finished then return false end
+	-- if enough time has passed since
+	-- the last input event, assume it’s 
+	-- a new swipe and reset the string
 	if not event or not event.button then return false end
 	if (AutoStyle=="single" or AutoStyle=="double") and event.PlayerNumber ~= mpn then return false	end
 
 	if event.type ~= "InputEventType_Release" then
-		if Handle[event.GameButton] then Handle[event.GameButton](event) end
+		if GetTimeSinceStart() - time > 0.03 or event.type == "InputEventType_Repeat" then
+			str = ""
+			if Handle[event.GameButton] then Handle[event.GameButton](event) end
+			MESSAGEMAN:Broadcast("PotentialSwipe", {PotentialSwipe=false})
+		else
+			MESSAGEMAN:Broadcast("PotentialSwipe", {PotentialSwipe=true})
+		end
+		time = GetTimeSinceStart()
+		if ToEnumShortString( event.DeviceInput.button ) ~= "right shift" then
+			if ToEnumShortString( event.DeviceInput.button ) ~= "/" then
+				str = str .. ToEnumShortString( event.DeviceInput.button )
+				if ToEnumShortString( event.DeviceInput.button ) == "enter" then
+					MESSAGEMAN:Broadcast("Swipe", {hash=sha256(str)})
+				end
+			end
+		end
 	end
 end
 
