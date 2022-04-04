382c392dafc295a68166529bc4d3e3115ecf402c
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2022-04-04 13:52:04 -0400

    UPSRT2.5 support

--- Modules/UnlockSystem.lua
+++ Modules/UnlockSystem.lua
@@ -1,5 +1,6 @@
 local t = {}
 local machinePath = ""
+--local apple = ""
 function getPlayersProfilePath(player)
 	if not player then return end
 	
@@ -7,13 +8,13 @@ function getPlayersProfilePath(player)
 	local dir = ""
 	if ToEnumShortString(player) == "P1" then
 		if PROFILEMAN:IsPersistentProfile(PLAYER_1) then
-			return PROFILEMAN:GetProfileDir(profile_slot[1])
+			return PROFILEMAN:GetProfileDir(profile_slot[1])..'/Unlock System/'
 		else
 			return machinePath
 		end
 	else
 		if PROFILEMAN:IsPersistentProfile(PLAYER_2) then
-			dir  = PROFILEMAN:GetProfileDir(profile_slot[2])
+			dir  = PROFILEMAN:GetProfileDir(profile_slot[2])..'/Unlock System/'
 		else
 			return machinePath
 		end
@@ -23,15 +24,15 @@ end
 
 function SongUnlockCheck(songName)
 	for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-		local Song = getPlayersProfilePath(player)..'Unlock System/Unlocks.lua' 
-		if PassFileExists(Song) then
+		local Song = getPlayersProfilePath(player)..'/Unlocks.lua' 
+		if UnlockFileExists(Song) then
 			if UnlockFileCheck(Song, songName) then return true end
 		end
 	end
     return false
 end
 
-function PassFileExists(filePath)
+function UnlockFileExists(filePath)
 	if FILEMAN:DoesFileExist(filePath) then
 		return true
 	end
@@ -53,9 +54,20 @@ end
 
 function LockUnlockCheck()
 	groupName = "Squeaky Beds and Leaky Faucets (Nightly Beta)"
+	for i, song in ipairs(SONGMAN:GetSongsInGroup("UPSRT2.5 - Locked Away")) do
+		machinePath = song:GetSongDir() .. "../Unlock System/"
+        if song:GetOrigin() ~= "" then
+			--apple = apple .. song:GetOrigin().. " ".. song:GetDisplayMainTitle() .. "\n"
+			if SongUnlockCheck(song:GetDisplayMainTitle()) then
+				UNLOCKMAN:UnlockEntryID(song:GetOrigin())
+			else 
+				UNLOCKMAN:LockEntryID(song:GetOrigin())
+			end
+		end	
+	end	
 
 	for i, song in ipairs(SONGMAN:GetSongsInGroup(groupName)) do
-		machinePath = song:GetSongDir() .. "../"
+		machinePath = song:GetSongDir() .. "../Unlock System/"
         if song:GetOrigin() ~= "" then
 			if SongUnlockCheck(song:GetDisplayMainTitle()) then
            		UNLOCKMAN:UnlockEntryID(song:GetOrigin())
@@ -63,7 +75,6 @@ function LockUnlockCheck()
 				UNLOCKMAN:LockEntryID(song:GetOrigin())
 			end
 		end
-		
 	end	
 end
 
