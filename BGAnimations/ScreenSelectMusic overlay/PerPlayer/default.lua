0479fff1de987ee7aa3fc140036d42c0a3922e91
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2022-04-04 15:24:01 -0400

    I think this is the last major bit of stuff to have merged over from upstream.
    So in upstream selectmusic displays a lot more and it can be intimidating for newer players and takes a lot of the surprise out of playing new charts. I've moved all of the new display to FA+ mode and ITG remains at its default

--- BGAnimations/ScreenSelectMusic overlay/PerPlayer/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/PerPlayer/default.lua
@@ -4,14 +4,25 @@ local t = Def.ActorFrame{}
 -- If the other player suddenly latejoins, we can't dynamically add more actors to the screen
 -- We can only unhide hidden actors that were there all along
 for player in ivalues( PlayerNumber ) do
-	t[#t+1] = LoadActor("./DensityGraph.lua", player)
+	if SL.Global.GameMode == "FA+" then
+		t[#t+1] = LoadActor("./DensityGraph.lua", player)
+	end
 	-- AuthorCredit, Description, and ChartName associated with the current stepchart
-	t[#t+1] = LoadActor("./StepArtist.lua", player)
+	if SL.Global.GameMode == "FA+" then
+		t[#t+1] = LoadActor("./StepArtist.lua", player)
+	else
+		t[#t+1] = LoadActor("./StepArtist-ITG.lua", player)
+	end
+
 end
 
 -- Bouncing cursor inside the grid of difficulty blocks. These should be on top of both of the other elements.
 for player in ivalues( PlayerNumber ) do
-	t[#t+1] = LoadActor("./Cursor.lua", player)
+	if SL.Global.GameMode == "FA+" then
+		t[#t+1] = LoadActor("./Cursor.lua", player)
+	else
+		t[#t+1] = LoadActor("./Cursor-ITG.lua", player)
+	end
 end
 
 return t
--- BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/default.lua
@@ -2,8 +2,9 @@ local file
 
 if GAMESTATE:IsCourseMode() then
 	file = LoadActor("./CourseContentsList.lua")
+elseif SL.Global.GameMode == "ITG" then
+	file = LoadActor("./Grid-ITG.lua")
 else
 	file = LoadActor("./Grid.lua")
 end
-
 return file
