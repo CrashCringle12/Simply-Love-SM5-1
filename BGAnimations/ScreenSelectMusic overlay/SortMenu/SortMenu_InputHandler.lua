500cdf2032be49bb1a199d409ec842eb96c0cc71
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2022-05-09 11:30:34 -0400

    Allow the player to switch to Casual mode from ITG/FA+ mode if no stages have been played.
    (cherry picked from commit c9ce9fcbaca6e65373a83ee5dc1779abb98593d1)
    
    # Conflicts:
    #	BGAnimations/ScreenSelectMusic overlay/SortMenu/SortMenu_InputHandler.lua

--- BGAnimations/ScreenSelectMusic overlay/SortMenu/SortMenu_InputHandler.lua
+++ BGAnimations/ScreenSelectMusic overlay/SortMenu/SortMenu_InputHandler.lua
@@ -1,30 +1,23 @@
 local sort_wheel = ...
-
 -- this handles user input while in the SortMenu
 local input = function(event)
 	if not (event and event.PlayerNumber and event.button) then
 		return false
 	end
 	SOUND:StopMusic()
-
 	local screen   = SCREENMAN:GetTopScreen()
 	local overlay  = screen:GetChild("Overlay")
 	local sortmenu = overlay:GetChild("SortMenu")
-
 	if event.type ~= "InputEventType_Release" then
-
 		if event.GameButton == "MenuRight" or event.GameButton == "MenuDown" then
 			sort_wheel:scroll_by_amount(1)
 			sortmenu:GetChild("change_sound"):play()
-
 		elseif event.GameButton == "MenuLeft" or event.GameButton == "MenuUp" then
 			sort_wheel:scroll_by_amount(-1)
 			sortmenu:GetChild("change_sound"):play()
-
 		elseif event.GameButton == "Start" then
 			sortmenu:GetChild("start_sound"):play()
 			local focus = sort_wheel:get_actor_item_at_focus_pos()
-
 			if focus.kind == "SortBy" then
 				if focus.sort_by == "Favorites" then
 					SCREENMAN:GetTopScreen():GetMusicWheel():ChangeSort("SortOrder_Preferred")
@@ -41,16 +34,17 @@ local input = function(event)
 				SL.Global.GameMode = focus.change
 				SetGameModePreferences()
 				THEME:ReloadMetrics()
-
 				-- Broadcast that the SL GameMode has changed
 				-- SSM's header will update its text and highscore names in the PaneDisplays will refresh
 				MESSAGEMAN:Broadcast("SLGameModeChanged")
-
 				-- Reload the SortMenu's available options and queue "DirectInputToEngine"
 				-- to return input from Lua back to the engine and hide the SortMenu from view
 				sortmenu:playcommand("AssessAvailableChoices"):queuecommand("DirectInputToEngine")
-
-
+				-- the player is switching to casual mode which uses a different SelectMusic screen
+				if focus.change == "Casual" then
+					screen:SetNextScreenName("ScreenSelectMusicCasual")
+					screen:StartTransitioningScreen("SM_GoToNextScreen")
+				end
 			-- the player wants to change styles, for example from single to double
 			elseif focus.kind == "ChangeStyle" then
 				-- If the MenuTimer is in effect, make sure to grab its current
@@ -58,24 +52,18 @@ local input = function(event)
 				if PREFSMAN:GetPreference("MenuTimer") then
 					overlay:playcommand("ShowPressStartForOptions")
 				end
-
 				-- Get the style we want to change to
 				local new_style = focus.change:lower()
-
 				-- accommodate techno game
 				if GAMESTATE:GetCurrentGame():GetName()=="techno" then new_style = new_style.."8" end
-
 				-- set it in the engine
 				GAMESTATE:SetCurrentStyle(new_style)
-
 				-- finally, reload the screen
 				screen:SetNextScreenName("ScreenReloadSSM")
 				screen:StartTransitioningScreen("SM_GoToNextScreen")
-
 			elseif focus.new_overlay then
 				if focus.new_overlay == "TestInput" then
 					sortmenu:queuecommand("DirectInputToTestInput")
-
 				elseif focus.new_overlay == "Leaderboard" then
 					-- The leaderboard entry is removed altogether if the service isn't available.
 					sortmenu:queuecommand("DirectInputToLeaderboard")
@@ -99,8 +87,6 @@ local input = function(event)
 			overlay:queuecommand("DirectInputToEngine")
 		end
 	end
-
 	return false
 end
-
 return input
