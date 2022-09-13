2703c15ede406dd6b5b20a351439f8f4bc149112
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2022-09-13 00:24:43 -0400

    Begin creation of discord presence module

---
+++ Modules/DiscordPresence.lua
@@ -0,0 +1,47 @@
+local t = {}
+local discordPresence = {
+    --startTime = os.time(),
+    lastSeenSong = "",
+    smalltext = "",
+    state = "",
+    details = "",
+    largetext = "",
+}
+t["ScreenSelectMusic"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+		discordPresence.smalltext = GAMESTATE:IsEventMode() and "Event Mode" or GAMESTATE:GetCoins().." credits remaining..."
+		discordPresence.state = GAMESTATE:IsCourseMode() and "Playing ".. SL.Global.GameMode.." "..GAMESTATE:GetCurrentStyle():GetName().. "s marathon" or "Playing ".. SL.Global.GameMode.." "..GAMESTATE:GetCurrentStyle():GetName().. "s"
+		if not transitioning_out then
+			GAMESTATE:UpdateDiscordPresenceDetails("Stage: "..(GAMESTATE:GetCurrentStageIndex()+1) .." || "..GAMESTATE:GetNumSidesJoined().." Player",discordPresence.smalltext,"default","selectmusic","Selecting Music...",discordPresence.state, discordPresence.startTime)
+		else
+			discordPresence.startTime = os.time()
+			GAMESTATE:UpdateDiscordPresenceDetails("Stage: "..(GAMESTATE:GetCurrentStageIndex()+1) .." || "..GAMESTATE:GetNumSidesJoined().." Player",discordPresence.smalltext,"default","selectmusic","Selecting Music...",discordPresence.state, discordPresence.startTime)
+        end
+    end 
+}
+t["ScreenTitleMenu"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+        GAMESTATE:UpdateDiscordMenu("Title Menu: Simply "..ThemePrefs.Get("VisualStyle"))
+    end
+}
+t["ScreenEdit"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+        SM(groupName)
+        GAMESTATE:UpdateDiscordPresenceDetails("Working in the ".. groupName .." pack.","Current beat: ".. currBeat.."/".. lastBeat .." ("..bpm.."bpm)", "default", "editing", "Currently in Edit Mode..", "Editing a ".. style  .. "s chart",discordPresence.startTime)
+    end
+}
+t["ScreenEditMenu"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+        SM(groupName)
+        GAMESTATE:UpdateDiscordPresence("This is where the magic happens...", "Currently in Edit Mode..", "Browsing the Edit Menu",discordPresence.startTime)    
+    end
+}
+t["ScreenPlayerOptions"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+        SM(groupName)
+        GAMESTATE:UpdateDiscordPresenceDetails("Song: "..song:GetDisplayMainTitle().." | Stage: "..GAMESTATE:GetCurrentStageIndex()+1,GAMESTATE:GetNumSidesJoined().." Player || "..discordPresence.smalltext,"default","selectmusic","Entering Players Options...",discordPresence.state, discordPresence.startTime)
+    end
+}
+
+
+return t
