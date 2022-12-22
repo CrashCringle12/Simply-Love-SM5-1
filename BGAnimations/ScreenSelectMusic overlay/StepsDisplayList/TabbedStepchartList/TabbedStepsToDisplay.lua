f57e0cd3d44fab452a49bdfd5f88e0c07749d014
quietly-turning <quietly.turning@gmail.com>
2022-12-22 02:40:33 -0500

    fix autosetstyle's tabs in SSM for 4:3
    This fixes the tabbed layout in ScreenSelectMusic when autosetstyle is enabled
    to work in 4:3, 16:10, and 16:9 aspect ratios.
    
    21:9 is also fine, but looks awkward and could make better use of the space.
    But, that's a different commit for a different day.

--- BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/TabbedStepchartList/TabbedStepsToDisplay.lua
+++ BGAnimations/ScreenSelectMusic overlay/StepsDisplayList/TabbedStepchartList/TabbedStepsToDisplay.lua
@@ -1,17 +1,21 @@
+local num_tabs = ...
+
 return function(player, AllSteps)
 	local song = GAMESTATE:GetCurrentSong()
 	AllSteps = AllSteps or (song and SongUtil.GetPlayableSteps(song)) or {}
 
-	if #AllSteps < 10 then
+	-- if there are fewer playable stepcharts than the UI accommodates, no problem, return them all
+	if #AllSteps < num_tabs then
 		local t = {}
-		local _i = 9
-		for i=1,10 do
+		local _i = num_tabs-1
+		for i=1,num_tabs do
 			t[i] = AllSteps[#AllSteps-_i]
 			_i = _i - 1
 		end
 		return t
 	end
 
+	-- otherwise,
 	player = player or GAMESTATE:GetMasterPlayerNumber()
 	if not player then return {} end
 
@@ -23,15 +27,15 @@ return function(player, AllSteps)
 	local _i
 
 
-	if i < 6 then
+	if i <= 5 then
 		_i = 1
 	elseif i > 5 and i < #AllSteps-5 then
 		_i = i-5
 	else
-		_i=#AllSteps-9
+		_i=#AllSteps-(num_tabs-1)
 	end
 
-	for index=1,10 do
+	for index=1,num_tabs do
 		t[index] = AllSteps[_i]
 		_i = _i + 1
 	end
